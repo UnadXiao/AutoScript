b64d034b408d090d68d91506b670f40671ad5745
MarkXiao <xiaozp@daheng-image.com>
2018-04-27 18:32:50 +0800

    Update LookGUID

--- AutoStorePDB/LookGUID.bat
+++ AutoStorePDB/LookGUID.bat
@@ -1,24 +1,19 @@
 :: 
 :: 功能：查看路径下exe/dll的GUID信息
 :: 前提：在cmd中可以运行dumpbin命令
-:: 版本：v1.0
+:: 版本：v1.1
 ::
 
 @echo off
 
 :: 下面内容根据实际需要修改
-set "dir=*.*"
-set "tmpfile=.tmp"
+set "filestyle="exe dll""
 
-echo 当前批处理全路径：%~dp0
-if exist %tmpfile% del %tmpfile%
-
-for %%i in (%dir%) do (
-dumpbin /HEADERS %%i | findstr /r /c:"RSDS.*{.*}" > %tmpfile%
-for /f "tokens=2 delims=," %%j in (%tmpfile%) do (
-echo %%i%%~j
+echo 当前批处理路径：%~dp0
+for /f %%i in ('dir /b ^| findstr %filestyle%') do (
+for /f "tokens=2 delims=," %%j in ('dumpbin /HEADERS %%i ^| findstr /r /c:"RSDS.*{.*}"') do (
+echo %%i%%j
 )
 )
 
-del %tmpfile%
 set /p prompt=按回车退出
